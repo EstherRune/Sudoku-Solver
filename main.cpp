4b6a8c86643f44d47ef32f09690f056918c21617
erikmlundquist <erikmlundquist@gmail.com>
2015-11-05 16:51:01 -0500

    everything but positional check is filled out

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@ Sudoku Solver by Erik Lundquist
 */
 
 #include <iostream>
-#include <math.h>
+
 using namespace std;
 
 int Puzzle[9][9][10];
+
+//returns true if cell has a solution set
+bool is_set(int x, int y){
+  if(Puzzle[x]y[][0] != 0) return true;
+  else return false;
+}
 
 //fills Puzzle with all true possible values and zeroes the actual values
 void init_puzzle(){
@@ -73,43 +79,157 @@ void enter_puzzle(){
   }
 }
 
-
-
-
-//called to manage solving of puzzle
-bool solve_puzzle(){
-
-  return true;
-}
-
-//checks a single position on the board for collisions and falsifies impossible answers
-void single_pos_check(int x, int y){
-
-}
-
 //checks a single position to see if there is only one number it can be
-void only_ans_check(int x, int y){
-
-}
-
-//check a single position to see if it is the only place a number can go in its squar/horizontal/vertical
-void only_pos_check(int x, int y){
-
-}
-
-//does a single position check for the whole board
-void whole_puzzle_check(){
-
+int only_ans_check(int x, int y){
+  int possibilities = 0;
+  int solution;
+  for(int z = 1; z < 10; z++){
+    if(Puzzle[x][y][z] == 1){
+      possibilities++;
+      solution = z;
+    }
+  }
+  if(possibilities == 0) return 0; //There should never be no possible answer for a cell
+  else if(possibilities == 1){
+    Puzzle[x][y][0] = solution; //If there is only one possibility, set the answer for the cell
+    return 1
+  }
+  else return 1;
 }
 
 //checks every position to see if there is only one possible number it can be
-void only_ans_sweep(){
+int only_ans_sweep(){
+  for(int y=0; y < 9; y++){
+    for(int x=0; x < 9; x++){
+      int check = only_ans_check(x,y);
+      if(check == 0) return 0; //Pass that error up!
+    }
+  }
+  return 1;
+}
 
+//check a single position to see if it is the only place a number can go in its squar/horizontal/vertical
+int only_pos_check(int x, int y){
+
+
+  return 1;
 }
 
 //checks every position to see if it is the only place a number in a sqaure/horizontal/vertical can go
-void only_pos_sweep(){
+int only_pos_sweep(){
+  for(int y=0; y < 9; y++){
+    for(int x=0; x < 9; x++){
+      int check = only_pos_check(x,y);
+      if(check == 0) return 0;
+    }
+  }
+  return 1;
+}
 
+//checks a single position on the board for collisions and falsifies impossible answers
+int single_cell_check(int x, int y){
+
+  possibilities[10];
+  for(int i = 0; i < 10; i++){
+    possibilities[i] = 1; //we set a temp variable to have all possible, then we check everywhere and update
+  }
+
+  //horizontal check
+  for(int i = 0; i < 9; i++){
+    //only check if it isn't itself
+    if(i != x) possibilities[ Puzzle[i][y][0] ] = 0;
+  }
+
+  //vertical check
+  for(int i = 0; i < 9; i++){
+    //only check if it isn't itself
+    if(i != y) possibilities[ Puzzle[x][i][0] ] = 0;
+  }
+
+  //sqaure check
+  //0-2, 3-5, 6-8 are the possible squares
+  int x_square;
+  int y_square;
+
+  //determine what square we are in and set starting points for x and y
+  if(x < 3) x_sqaure = 0;
+  else if(x > 5) x_square = 6;
+  else x_square = 3;
+  if(y < 3) y_sqaure = 0;
+  else if(y > 5) y_square = 6;
+  else y_square = 3;
+
+  //now go through sqaure for the check
+  for(int i = x_square; i < (x_sqaure+3); i++){
+    for(int j = y_square; j < (y_sqaure+3); j++){
+      //we only check the x,y pairs that are in the square and not in a row/column because we already checked those
+      if(i != x && j != y) possibilities[ Puzzle[i][j][0] ] = 0;
+    }
+  }
+
+  //now we have an array with the possible things the cell can be in locations 1-9
+  //let's set the Puzzle to reflect our findings
+  for(int i = 1; i < 10; i++){
+    Puzzle[x][y][i] = possibilities[i]; //put 0 or 1 in each corrisponding location
+  }
+
+  //we do an error check here too
+  //if the cell is set and it can't be the number it currently is
+  //we give up and show the puzzle as is
+  if(is_set(x,y)){
+    if( possibilities[ Puzzle[x][y][0] ] == 0) return 0;
+  }
+
+  return 1;
+}
+
+//does a single position check for the whole board
+int whole_puzzle_check(){
+  for(int y=0; y < 9; y++){
+    for(int x=0; x < 9; x++){
+      int check = single_cell_check(x,y);
+      if(check == 0) return 0;
+    }
+  }
+  return 1;
+}
+
+int complete(){
+  int check = whole_puzzle_check();
+  if(check == 0) return 0;
+
+  for(int y=0; y < 9; y++){
+    for(int x=0; x < 9; x++){
+      if(!is_set(x,y)) return 1;
+    }
+  }
+
+  return 2;
+}
+
+//called to manage solving of puzzle
+int solve_puzzle(){
+  bool working = true;
+  int check;
+  while(working){
+
+      //check if the puzzle is full
+      //this calls whole_puzzle check which updates possible answers for each cell while checking
+      //2 if it is full and no error
+      //1 if isnt full
+      //0 if error is found
+      int done = complete();
+      if(done == 2) return 1; //full and no error? WE SOLVED IT!
+      else if(done == 0) return 0; //shit...collision
+
+      //go through whole puzzle and set cells that can only be one thing
+      check = only_ans_sweep();
+      if(check == 0) return 0;
+
+
+  }
+
+  return 1; //SOLVED!
 }
 
 main (){
@@ -118,17 +238,21 @@ main (){
     cout << "Hello! Welcome to Sudoke Solver!" << endl;
     init_puzzle();
     enter_puzzle();
-    print_puzzle();
     cout << "Solving!" << endl;
-    bool solved = solve_puzzle();
-    if(solved){
-      print_puzzle();
-      cout << endl << "You're puzzle has been solved!" << endl;
-      cout << "Would you like to solve another puzzle? (y/n): ";
-      char ans;
-      cin >> ans;
-      if(ans != 'y') run = false;
+    int check = solve_puzzle();
+    if(check == 1){
+      cout << endl << "Your puzzle has been solved!" << endl;
     }
+    else{
+      cout << "We couldn't solve your puzzle." <<endl;
+      cout << "This was the end result, but an error occurred along the way." << endl;
+      cout
+    }
+    print_puzzle();
+    cout << "Would you like to solve another puzzle? (y/n): ";
+    char ans;
+    cin >> ans;
+    if(ans != 'y') run = false;
   }
   return 0;
 }
